23f37c387676b19987a2529efa335e1f0e103c7b
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-01-10 11:50:38 -0700

    card image responsiveness

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { 
     Grid,
     Card, 
@@ -14,12 +15,17 @@ export default function PostCard({post}) {
         <Grid item xs={12} md={6} lg={4} key={post.pk}>
             <Card>
                 <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="300"
-                        image={`http://catstagram.lofty.codes/media/${post.image}`}
-                        alt={post.name}
-                    />
+                    <CardMedia sx={{ minHeight: 250, position: 'relative' }}>
+                            <Image 
+                                src={`http://catstagram.lofty.codes/media/${post.image}`}
+                                alt={post.name}
+                                style={{ objectFit: 'cover'}}
+                                fill
+                                sizes="(max-width: 768px) 66vw,
+                                        (max-width: 1200px) 50vw,
+                                        33vw"
+                            />
+                    </CardMedia>
                     <CardContent>
                         <Typography variant="body2" component="div">
                             <Link href={`/post/${post.pk}`}>{post.pk}</Link>
