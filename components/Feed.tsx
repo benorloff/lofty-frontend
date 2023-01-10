bad9437ceeb1de96aa886b250c6241955364e354
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-01-09 21:39:43 -0700

    responsive breakpoints in feed grid

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -20,29 +20,24 @@ export default function Feed() {
     const fetcher = async (url: string) => fetch(url).then(res => res.json())
     const { data, error } = useSWR(url, fetcher);
 
-    if (error) <p>Loading failed...</p>;
-    if (!data) <h1>Loading...</h1>;
+    if (error) return <p>Loading failed...</p>;
+    if (!data) return <h1>Loading...</h1>;
 
     return (
         <Grid container spacing={2}>
             { data && 
                 data.map((post) => (
                     <Grid item xs={12} md={6} lg={4} key={post.pk}>
-                        {/* <img
-                            src={`http://catstagram.lofty.codes/media/${post.image}`}
-                            alt={post.name}
-                            height={100}
-                        /> */}
                         <Card>
                             <CardActionArea>
                                 <CardMedia
                                     component="img"
-                                    height="150"
+                                    height="300"
                                     image={`http://catstagram.lofty.codes/media/${post.image}`}
                                     alt={post.name}
                                 />
                                 <CardContent>
-                                    <Typography variant="h5" component="div">
+                                    <Typography variant="body" component="div">
                                         {post.name}
                                     </Typography>
                                 </CardContent>
