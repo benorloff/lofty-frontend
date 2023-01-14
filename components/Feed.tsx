a2b1206d57c9589a89b6260c4315a2cc51784748
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-01-13 17:46:17 -0700

    staticProps for post route

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,73 +1,17 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import { 
     Container,
     Grid, 
-    CardContent, 
-    CardMedia, 
-    Typography, 
-    CardActionArea, 
-    IconButton, 
-    ImageList, 
-    ImageListItem, 
-    ImageListItemBar, 
-    Skeleton
 } from '@mui/material'
-import { Info } from '@mui/icons-material'
 import PostCard  from './Card'
-import useSWR from 'swr'
 
 export default function Feed({ posts }) {
-
-    const url = 'http://catstagram.lofty.codes/api/posts/'
-    const fetcher = async (url: string) => fetch(url).then(res => res.json())
-    const { data, error } = useSWR(url, fetcher);
-
-    console.log(data, '<-- data')
-
-    if (error) return <p>Loading failed...</p>;
-    if (!data) return <h1>Loading...</h1>;
-
-    // Change grid to masonry??
 
     return (
         <Container maxWidth="lg" sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-                { posts ? ( 
-                    posts.map((post) => (
-                        <PostCard post={post} key={post.pk}/>
-                        // <Grid item xs={12} md={6} lg={4} key={post.pk}>
-                        //     <Card>
-                        //         <CardActionArea>
-                        //             <CardMedia
-                        //                 component="img"
-                        //                 height="300"
-                        //                 image={`http://catstagram.lofty.codes/media/${post.image}`}
-                        //                 alt={post.name}
-                        //             />
-                        //             <CardContent>
-                        //                 <Typography variant="body2" component="div">
-                        //                     <Link href={`/post/${post.pk}`}>{post.pk}</Link>
-                        //                 </Typography>
-                        //             </CardContent>
-                        //         </CardActionArea>
-                        //     </Card>
-                        // </Grid>
-                        
-                    ))
-                ) : (
-                    <>
-                        <Skeleton variant='rectangular' sx={{ bgcolor: '#333333' }} width={300} height={300} />
-                        <Skeleton variant='rectangular' sx={{ bgcolor: '#333333' }} width={300} height={300} />
-                        <Skeleton variant='rectangular' sx={{ bgcolor: '#333333' }} width={300} height={300} />
-                        <Skeleton variant='rectangular' sx={{ bgcolor: '#333333' }} width={300} height={300} />
-                        <Skeleton variant='rectangular' sx={{ bgcolor: '#333333' }} width={300} height={300} />
-                        <Skeleton variant='rectangular' sx={{ bgcolor: '#333333' }} width={300} height={300} />
-                        <Skeleton variant='rectangular' sx={{ bgcolor: '#333333' }} width={300} height={300} />
-                        <Skeleton variant='rectangular' sx={{ bgcolor: '#333333' }} width={300} height={300} />
-                        <Skeleton variant='rectangular' sx={{ bgcolor: '#333333' }} width={300} height={300} />
-                    </>
-                )
+                { posts.map((post) => (
+                    <PostCard post={post} key={post.pk}/> 
+                ))
                 }
             </Grid>
         </Container>
--- pages/post/[id]/index.tsx
+++ pages/post/[id]/index.tsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import Image from 'next/image'
 import { useSWRConfig } from 'swr'
-import { CommentsDisabled } from '@mui/icons-material'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { getAllPostIds } from '../../../lib/posts'
+import Header from '../../../components/Header'
+import { Container, Card, CardContent, Typography, Stack } from '@mui/material'
 
-export default function PostPage() {
+export default function PostPage({ post }) {
     const router = useRouter()
     const id = router.query.id as string
     // const { cache } = useSWRConfig()
@@ -20,104 +22,105 @@ export default function PostPage() {
     // const post = cachedPosts?.data?.find(({ pk }) => pk === Number(id))
     // console.log(post, '<-- post')
 
-    const [posts, setPosts] = useState([])
-    const [post, setPost] = useState(null)
+    // const [posts, setPosts] = useState([])
+    // const [post, setPost] = useState(null)
+    // const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        const getAllPosts = () => {
-            fetch('http://catstagram.lofty.codes/api/posts/')
-                .then((res) => res.json())
-                .then((data) => setPosts(data))
-        }
-        getAllPosts()
-    }, [])
+    // useEffect(() => {
+    //     const getAllPosts = () => {
+    //         fetch('http://catstagram.lofty.codes/api/posts/')
+    //             .then((res) => res.json())
+    //             .then((data) => setPosts(data))
+    //     }
+    //     getAllPosts()
+    // }, [])
 
-    useEffect(() => {
-        const getPost = () => {
-            const post = posts.find(({ pk }) => pk == id)
-            setPost(post)
-        }
-        getPost()
-    }, [posts])
+    // useEffect(() => {
+    //     const getPost = () => {
+    //         const post = posts.find(({ pk }) => pk === Number(id))
+    //         setPost(post)
+    //     }
+    //     getPost()
+    //     setLoading(false)
+    // }, [posts])
 
-    console.log(posts, '<-- posts from postpage')
-    console.log(post, '<-- post from postpage')
+    // console.log(posts, '<-- posts from postpage')
+    // console.log(post, '<-- post from postpage')
+
+    // if (loading) return <h1>Loading...</h1>
 
     return (
-        <div style={{ marginLeft: 25 }}>
-            { post ? ( 
-                <>
-                    <img width='150' src={`http://catstagram.lofty.codes/media/${post.image}`} />
-                    <div>PK: {post.pk}</div>
-                    <div>Name: {post.name}</div>
-                    <div>Created: {post.timestamp_created}</div>
-                    <div>Updated: {post.timestamp_updated}</div>
-                    {/* <div>Comments ({post.comments.length}):</div>
-                    <ul>
-                        { post.comments.map((c) => (
-                            <li key={c.pk}>{c.text}</li>
-                        ))}
-                    </ul> */}
-                </>
-            ) : (
-                <p>No post found</p>
-            )}
-            TEST
-            
-        </div>
+        <>
+            <Header />
+            <Container maxWidth="sm" sx={{ mt: 3 }}>
+                {/* <Image 
+                    src={`http://catstagram.lofty.codes/media/${post.image}`}
+                    alt={post.name}
+                    style={{ objectFit: 'contain'}}
+                    fill
+                    sizes="(max-width: 768px) 66vw,
+                            (max-width: 1200px) 50vw,
+                            33vw"
+                /> */}
+                <div>
+                    { post ? ( 
+                        <>
+                            <img width='100%' src={`http://catstagram.lofty.codes/media/${post.image}`} />
+                        </>
+                    ) : (
+                        <p>No post found</p>
+                    )}
+                
+                </div>
+                <Card>
+                    <CardContent>
+                        <Typography>Comments</Typography>
+                        <Stack>
+                            { post.comments ? (
+                                post.comments.map((c) => (
+                                    <div key={c.pk} >{c.text}</div>
+                                ))
+                            ) : (
+                                'No comments yet.'
+                            )
+
+                            }
+                        </Stack>
+                    </CardContent>
+                </Card>
+            </Container>
+        </>
     )
 }
 
-// export async function getStaticPaths () {
-//     let postIds = []
-//     const getAllPostIds = () => {
-//         const posts = fetch('http://catstagram.lofty.codes/api/posts/')
-//             .then((res) => res.json())
-//             .then((data) => postIds = data)
-//         return postIds.map((post) => {
-//             return {
-//                 params: {
-//                     id: post.pk,
-//                 }
-//             }
-//         })
-//     }
-//     getAllPostIds()
+export async function getStaticPaths() {
+    const posts = await fetch('http://catstagram.lofty.codes/api/posts/')
+        .then((res) => res.json())
+    const paths = posts.map((post) => {
+        return {
+            params: {
+                id: post.pk.toString(),
+            }
+        }
+    })
+    return {
+        paths,
+        fallback: false,
+    }
 
-//     // const posts = getAllPostIds();
-//     // console.log(posts, '<-- posts from getStaticPaths')
-    
-    
-//     // fetch('http://catstagram.lofty.codes/api/posts/')
-//     //     .then((res) => res.json())
+}
 
-//     // return {
-//     //     paths,
-//     //     fallback: false,
-//     // }
-// }
-
-// export async function getStaticProps ({ params }) {
-//     const posts = await fetch('http://catstagram.lofty.codes/api/posts/')
-//         .then((res) => res.json())
-//     const post = posts.find(({ pk }) => pk === params.id)
-//     return {
-//         props: {
-//             post,
-//             fallback: false,
-//         }
-//     }
-// }
-
-
-
-// interface Cache<Data> {
-//     get(key: string): Data | undefined
-//     set(key: string, value: Data): void
-//     delete(key: string): void
-//     keys(): IterableIterator<string>
-// }
-
-// interface Post<Object> {
-//     keys(): IterableIterator<string>
-// }
+export async function getStaticProps({ params }) {
+    const id = params.id
+    const posts = await fetch('http://catstagram.lofty.codes/api/posts/')
+        .then((res) => res.json())
+    const postData = posts.find(({ pk }) => pk === Number(id))
+    return {
+        props: {
+            post: {
+                id,
+                ...postData,
+            }
+        }
+    }
+}
