d3bf91d3b1f81da00fe5d5804bf9e1027c92a721
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-01-19 22:07:46 -0700

    upload form

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -6,21 +6,21 @@ import {
     DialogActions, 
     Button,
     TextField,
-
+    Stack,
     Typography 
 } from "@mui/material"
 import { ChangeEvent, useState } from "react"
 
 export default function UploadModal ({ open, handleClose }) {
 
-    const [image, setImage] = useState<File>()
+    const [selectedFile, setSelectedFile] = useState(null)
     const [description, setDescription] = useState('')
 
-    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setImage(e.target.files[0])
+    const handleFileChange = (e) => {
+        setSelectedFile(e.target.files[0])
     }
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (e) => {
         setDescription(e.target.value)
     }
 
@@ -33,24 +33,27 @@ export default function UploadModal ({ open, handleClose }) {
         >
             <DialogTitle>Upload Photo</DialogTitle>
             <DialogContent>
-                <Button
-                    variant="contained" component="label"
-                >
-                    Choose File
-                    <input hidden accept="image/*" type="file" />
-                </Button>
-                <TextField
-                    id="description"
-                    label="Description"
-                    value={description}
-                    onChange={handleChange}
-                >
+                <Stack spacing={2} sx={{ mt: 1 }}>
+                    <TextField
+                        required
+                        id="description"
+                        label="Description"
+                        value={description}
+                        onChange={handleChange}
+                    >
 
-                </TextField>
+                    </TextField>
+                    <input  
+                        required
+                        accept="image/*" 
+                        type="file"
+                        onChange={handleFileChange}
+                    />
+                </Stack>
             </DialogContent>
             <DialogActions>
                 <Button>Cancel</Button>
-                <Button>Upload</Button>
+                <Button variant="contained">Upload</Button>
             </DialogActions>
         </Dialog>
     )
