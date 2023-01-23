9a57359bfcf82316a5d419e338533bb0bc60ee51
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-01-22 19:49:17 -0700

    file upload functionality

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -7,23 +7,66 @@ import {
     Button,
     TextField,
     Stack,
-    Typography 
+    Typography,
+    CircularProgress
 } from "@mui/material"
 import { ChangeEvent, useState } from "react"
+import { useRouter } from "next/router"
 
 export default function UploadModal ({ open, handleClose }) {
 
-    const [selectedFile, setSelectedFile] = useState(null)
+    const router = useRouter()
+
+    const [file, setFile] = useState(null)
     const [description, setDescription] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [message, setMessage] = useState('')
 
     const handleFileChange = (e) => {
-        setSelectedFile(e.target.files[0])
+        setFile(e.target.files[0])
     }
 
     const handleChange = (e) => {
         setDescription(e.target.value)
     }
 
+    const handleUpload = async () => {
+
+        // Initiate loader
+        setLoading(true)
+
+        // Create form data for upload
+        const formData = new FormData()
+        formData.append('name', description)
+        formData.append('image', file)
+
+        setMessage('Sending your cat on a journey.')
+        
+        // Upload file to Lofty API
+        const uploadFile = await fetch('http://catstagram.lofty.codes/api/posts/', {
+            method: 'POST',
+            body: formData
+        })
+            .then((data) => {
+                setMessage('Success! Your cat arrived safely.')
+                // Close modal and reset state
+                setTimeout(() => {
+                    handleClose()
+                    setLoading(false)
+                    setMessage('')
+                }, 2000)
+                console.log(data, '<-- post api success')
+            })
+            .catch((error) => {
+                setMessage('Uh oh! There was problem sending your cat.')
+                console.log(error, '<-- post api error')
+            })
+        
+        // Send user back to feed
+        setMessage('Returning you to the land of cats.')
+        router.push('/')
+
+    }
 
 
     return (
@@ -33,27 +76,36 @@ export default function UploadModal ({ open, handleClose }) {
         >
             <DialogTitle>Upload Photo</DialogTitle>
             <DialogContent>
-                <Stack spacing={2} sx={{ mt: 1 }}>
-                    <TextField
-                        required
-                        id="description"
-                        label="Description"
-                        value={description}
-                        onChange={handleChange}
-                    >
+                { loading ? (
+                    <>
+                        <CircularProgress />
+                        <Typography variant="h6">
+                            {message}
+                        </Typography>
+                    </>
+                ) : (
+                    <Stack spacing={2} sx={{ mt: 1 }}>
+                        <TextField
+                            required
+                            id="description"
+                            label="Description"
+                            value={description}
+                            onChange={handleChange}
+                        >
 
-                    </TextField>
-                    <input  
-                        required
-                        accept="image/*" 
-                        type="file"
-                        onChange={handleFileChange}
-                    />
-                </Stack>
+                        </TextField>
+                        <input  
+                            required
+                            accept="image/*" 
+                            type="file"
+                            onChange={handleFileChange}
+                        />
+                    </Stack>
+                )}
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose}>Cancel</Button>
-                <Button variant="contained">Upload</Button>
+                <Button variant="contained" onClick={handleUpload}>Upload</Button>
             </DialogActions>
         </Dialog>
     )
