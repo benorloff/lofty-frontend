af01cb216fcc320676a27b3b9e7ae7b3a957bf29
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-01-23 15:08:46 -0700

    load more posts in feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,21 +1,48 @@
 import { 
     Container,
-    Grid, 
+    Grid,
+    Button, 
 } from '@mui/material'
 import PostCard  from './Card'
+import { useState } from 'react'
 
 export default function Feed({ posts }) {
 
-    const sortedPosts = posts.sort((a,b) => new Date(b.timestamp_created) - new Date(a.timestamp_created))
+    // Show 9 posts on initial render
+    const [postCount, setPostCount] = useState(9)
+
+    // Sort posts from most recent to least recent
+    const sortedPosts = posts.sort((a,b) => 
+        new Date(b.timestamp_created) - new Date(a.timestamp_created)
+    )
+
+    // Constrain posts to the current count
+    const displayedPosts = sortedPosts.slice(0, postCount)
+
+    const handleLoadMoreClick = () => {
+        setPostCount((prevValue) => prevValue + 9)
+    }
 
     return (
         <Container maxWidth="lg" sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-                { sortedPosts.map((post) => (
+                { displayedPosts.map((post) => (
                     <PostCard post={post} key={post.pk}/> 
                 ))
                 }
             </Grid>
+            { postCount < sortedPosts.length && 
+                <Grid 
+                    container 
+                    alignItems='center' 
+                    justifyContent='center'
+                    sx={{ m: 2 }}
+                >
+                    <Button variant='contained' onClick={handleLoadMoreClick}>
+                        Load More
+                    </Button>
+                </Grid>
+            }
         </Container>
     )
 }
