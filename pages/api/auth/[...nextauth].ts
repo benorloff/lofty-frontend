73d2edfaa3d023716b7d52390eeb8211b5de0b54
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-01-23 13:11:08 -0700

    add credentials provider auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,36 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ]
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password"},
+            },
+            async authorize(credentials) {
+                const authResponse = await fetch("http://catstagram.lofty.codes/api/users/login/", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(credentials),
+                })
+
+                if (!authResponse.ok) {
+                    return null
+                }
+
+                const user = await authResponse.json()
+
+                return user
+            },
+        }),
+    ],
 })
