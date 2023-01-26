92a96f32bebc039b9f72bc68fd57602ac5420901
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-01-26 15:08:46 -0700

    ts nocheck

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
